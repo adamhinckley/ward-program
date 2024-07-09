2041a8a8cbcd1c379c9a92f893d2fc434f1a1ec3
Adam Hinckley <adam.hinckley@clickbank.com>
2024-07-08 22:23:16 -0500

    Add drag & drop and update block titles, add icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import App from 'next/app';
 import { AppContextProvider } from '@/context/AppContext';
 
 const inter = Inter({ subsets: ['latin'] });

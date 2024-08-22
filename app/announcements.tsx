382f9f7ad6918caec6c8d385a9363ad5f585d942
Adam Hinckley <adam.hinckley@clickbank.com>
2024-08-21 22:33:52 -0500

    Move useEffect above early returns, update link color

--- app/announcements.tsx
+++ app/announcements.tsx
@@ -11,13 +11,22 @@ const announcementStyles = css`
 	}
 
 	a {
-		color: #007bff;
+		color: #1e40af;
 		text-decoration: underline;
 	}
 `;
 
 const Announcements = () => {
 	const { content } = useAppContext();
+
+	const announcementEl = document.getElementById('announcement') as HTMLElement;
+
+	useEffect(() => {
+		// add the html from content.announcements to the announcement element
+		if (announcementEl) {
+			announcementEl.innerHTML = content.announcements as string;
+		}
+	}, [content.announcements, announcementEl]);
 
 	if (Object.keys(content).length === 0) {
 		return null;
@@ -30,19 +39,6 @@ const Announcements = () => {
 	if (!announcementsAndLessons.length) {
 		return null;
 	}
-
-	const announcementEl = document.getElementById('announcement') as HTMLElement;
-
-	console.log('content.announcements', content.announcements);
-
-	useEffect(() => {
-		// add the html from content.announcements to the announcement element
-		if (announcementEl) {
-			announcementEl.innerHTML = content.announcements as string;
-		}
-	}, [content.announcements, announcementEl]);
-
-	console.log('content.announcements', content);
 
 	return <div className="mx-4" id="announcement" css={announcementStyles}></div>;
 };

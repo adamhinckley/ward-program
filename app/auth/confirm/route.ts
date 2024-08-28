29ef086deb715eef64c2263836dc4f419e0dfe17
Adam Hinckley <adam.hinckley@clickbank.com>
2024-08-28 08:49:38 -0500

    Add logs

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -8,15 +8,18 @@ export async function GET(request: NextRequest) {
 	const { searchParams } = new URL(request.url);
 	const token_hash = searchParams.get('token_hash');
 	const type = searchParams.get('type') as EmailOtpType | null;
-	const next = searchParams.get('next') ?? '/';
+	const next = searchParams.get('next') ?? '';
 
 	if (token_hash && type) {
 		const supabase = createClient();
 
-		const { error } = await supabase.auth.verifyOtp({
+		const { data, error } = await supabase.auth.verifyOtp({
 			type,
 			token_hash,
 		});
+
+		console.log('confirm user data', data);
+		console.log('confirm user error', error);
 		if (!error) {
 			// redirect user to specified redirect URL or root of app
 			redirect(next);

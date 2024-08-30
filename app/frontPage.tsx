31b8e89b69a0076288446af1b327401bcb2de646
Adam Hinckley <adam.hinckley@clickbank.com>
2024-08-30 17:46:12 -0500

    Fix image position

--- app/frontPage.tsx
+++ app/frontPage.tsx
@@ -5,13 +5,11 @@ import { useAppContext } from '../context/AppContext';
 const FrontPage = () => {
 	const { content } = useAppContext();
 	return (
-		<>
-			<div style={{ width: '100%' }}>
-				{content.imageUrl ? (
-					<Image src={content.imageUrl as string} alt="" width={550} height={550} />
-				) : null}
-			</div>
-		</>
+		<div style={{ width: 'fit-content', margin: '0 auto' }}>
+			{content.imageUrl ? (
+				<Image src={content.imageUrl as string} alt="" width={550} height={550} />
+			) : null}
+		</div>
 	);
 };
 export default FrontPage;

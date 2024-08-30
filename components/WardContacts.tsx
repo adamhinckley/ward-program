2c5c1bc9e74c3c92e09d85a5d23fb8e0609bc01e
Adam Hinckley <adam.hinckley@clickbank.com>
2024-08-30 18:18:38 -0500

    Don't load ward contacts component unless they exist

--- components/WardContacts.tsx
+++ components/WardContacts.tsx
@@ -19,27 +19,26 @@ const WardContacts = () => {
 	const { content } = useAppContext();
 	const { wardContacts } = content;
 
-	return (
+	return Array.isArray(wardContacts) && wardContacts.length ? (
 		<div css={styles}>
 			<Typography variant="h1">Ward Contacts</Typography>
-			{Array.isArray(wardContacts) &&
-				wardContacts.map((blockToNarrow, index) => {
-					const block = blockToNarrow as { left: string; right: string };
-					return (
-						<div className="agenda-block" key={index}>
-							<div className="title-container">
-								<p className="agenda-title">{block.left}</p>
-								<p className="agenda-content">
-									<a href={`tel:+1-${block.right}`} rel="noopener noreferrer">
-										{block.right}
-									</a>
-								</p>
-							</div>
+			{wardContacts.map((blockToNarrow, index) => {
+				const block = blockToNarrow as { left: string; right: string };
+				return (
+					<div className="agenda-block" key={index}>
+						<div className="title-container">
+							<p className="agenda-title">{block.left}</p>
+							<p className="agenda-content">
+								<a href={`tel:+1-${block.right}`} rel="noopener noreferrer">
+									{block.right}
+								</a>
+							</p>
 						</div>
-					);
-				})}
+					</div>
+				);
+			})}
 		</div>
-	);
+	) : null;
 };
 
 export default WardContacts;

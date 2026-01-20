5af4175f41b97c84323f1fbdf4a904f20436d769
Adam <adamhinckley@mac.com>
2026-01-20 15:25:57 -0600

    Remove  unused vars

--- app/demo/editor/page.tsx
+++ app/demo/editor/page.tsx
@@ -1,6 +1,4 @@
 import { AppContextProvider } from '@/context/AppContext';
-import { Edit } from '@mui/icons-material';
-import App from 'next/app';
 import Editor from '@/components/editor';
 import { initialStateForDemo } from '@/utils/helpers';
 

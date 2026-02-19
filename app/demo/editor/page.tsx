4e0fc634b43c255c3fa4193a2bac454859be0883
Adam <adamhinckley@mac.com>
2026-02-18 23:01:14 -0600

    refactor: remove dynamic export from Home component and update CI environment variables

--- app/demo/editor/page.tsx
+++ app/demo/editor/page.tsx
@@ -2,8 +2,6 @@ import { AppContextProvider } from '@/context/AppContext';
 import Editor from '@/components/editor';
 import { initialStateForDemo } from '@/utils/helpers';
 
-export const dynamic = 'force-dynamic';
-
 export default function Home() {
 	return (
 		<AppContextProvider initialState={initialStateForDemo}>

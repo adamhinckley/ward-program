b505b018a8fc79101f5244d833ff9c6338367625
Adam <adamhinckley@mac.com>
2026-02-18 22:55:22 -0600

    feat: add dynamic export to Home component for improved rendering

--- app/demo/editor/page.tsx
+++ app/demo/editor/page.tsx
@@ -2,6 +2,8 @@ import { AppContextProvider } from '@/context/AppContext';
 import Editor from '@/components/editor';
 import { initialStateForDemo } from '@/utils/helpers';
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
 	return (
 		<AppContextProvider initialState={initialStateForDemo}>

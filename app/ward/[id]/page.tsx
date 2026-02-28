fd00ddb5c5440722c424b5a3ddfc645ca6c0f892
Adam <adamhinckley@mac.com>
2026-02-28 08:05:53 -0600

    fix: update to static literal to fix the build

--- app/ward/[id]/page.tsx
+++ app/ward/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { WardProgramPage } from '@/app/ward/WardProgramPage';
 
-export const revalidate = 60 * 60;
+export const revalidate = 3600;
 
 export default async function WardPage({ params }: { params: Promise<{ id: string }> }) {
 	const { id } = await params;
